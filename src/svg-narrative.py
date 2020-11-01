a2d961ba9be5fa7ef47a1c84ad21b33f0b8ba965
Aslak Johansen <aslakj@gmail.com>
2020-11-01 18:54:31 +0100

    Updates from the innose webinar work

--- src/svg-narrative.py
+++ src/svg-narrative.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 
-def set_display (e, display):
+def set_style_attr (e, key, value):
+    if not 'style' in e.attrs: e.attrs['style'] = ''
+    
     # read
     style = e.attrs['style'].split(';')
     props = {}
@@ -10,10 +12,45 @@ def set_display (e, display):
         props[elements[0]] = elements[1]
     
     # modify
-    props['display'] = display
+    if value==None:
+        if key in props: del props[key]
+    else:
+        props[key] = value
     
     # write
     e.attrs['style'] = ';'.join(map(lambda k: '%s:%s'%(k, props[k]), props.keys()))
+
+def set_stroke_color (e, color):
+    if e.name=='g':
+        for child in e.children:
+            if child!=None:
+                set_stroke_color(child, color)
+    elif e.name in ['path', 'rect', 'text', 'ellipse']:
+        set_style_attr(e, 'stroke', color)
+    else:
+        print('Warning: Don\'t know how to set stroke color for tag type "%s"' % e.name)
+
+def set_end_marker (e, marker):
+    set_style_attr(e, 'marker-end', marker)
+
+def set_fill_color (e, color):
+    set_style_attr(e, 'fill', color)
+
+def set_display (e, display):
+    set_style_attr(e, 'display', display)
+
+def highlight (e, color):
+    if e.name=='path':
+        set_stroke_color(e, color)
+    elif e.name=='rect':
+        set_stroke_color(e, color)
+    elif e.name=='text':
+        set_fill_color(e, color)
+    elif e.name=='g':
+        for child in e.children:
+            highlight(child, color)
+    else:
+        print('Error: Don\'t know how to highlight "%s". Skipping ...' % e.name)
 
 class Model:
     def __init__ (self, filename: str):
@@ -48,12 +85,46 @@ class Model:
         result = True
         
         for key in idmap:
-            value = idmap[key]
-            e = self.root.find(id=value)
-            if not silent: print('Value "%s" of key "%s"%s found'%(value, key, " not" if e==None else ""))
-            if key==None: result = False
+            values = idmap[key]
+            if type(values)!=list:
+                values = [values]
+            for value in values:
+                e = self.root.find(id=value)
+                if not silent: print('Value "%s" of key "%s"%s found'%(value, key, " not" if e==None else ""))
+                if e==None: result = False
         
         return result
+    
+    def fill (self, ids, color):
+        if type(ids)==str:
+            ids = [ids]
+        for identifier in ids:
+            e = self.root.find(id=identifier)
+            set_fill_color(e, color)
+    
+    def stroke (self, ids, color):
+        if type(ids)==str:
+            ids = [ids]
+        for identifier in ids:
+            e = self.root.find(id=identifier)
+            set_stroke_color(e, color)
+    
+    def highlight (self, ids, highlight_color):
+        if type(ids)==str:
+            ids = [ids]
+        for identifier in ids:
+            e = self.root.find(id=identifier)
+            highlight(e, highlight_color)
+    
+    def lowlight (self, ids):
+        pass
+    
+    def set_end_marker (self, ids, marker):
+        if type(ids)==str:
+            ids = [ids]
+        for identifier in ids:
+            e = self.root.find(id=identifier)
+            set_end_marker(e, marker)
 
 if __name__ == "__main__":
     ids = {
